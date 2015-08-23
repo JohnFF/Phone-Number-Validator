934e778e3d9bb414a8eca3d74d4c8f3e920f6617
John Kirk <john@civifirst.com>
2015-08-23 11:29:43 +0100

    Neatening

--- CRM/Phonenumbervalidator/Utils.php
+++ CRM/Phonenumbervalidator/Utils.php
@@ -10,42 +10,42 @@ class CRM_Phonenumbervalidator_Utils {
   public static function getPhoneNumberRegexes(){
     return array(
       'Australia' => array(
-        array('label' => 'Australian landlines (national)',         'regex' => '^0[^4][0-9]{8}$'),
-        array('label' => 'Australian mobiles (national)',           'regex' => '^04[0-9]{8}$'),
+        array('label' => 'Australian landlines (national)',      'regex' => '^0[^4][0-9]{8}$'),
+        array('label' => 'Australian mobiles (national)',        'regex' => '^04[0-9]{8}$'),
         array('label' => 'Australian landlines (international)', 'regex' => '^0061[^4][0-9]{8}$'),
         array('label' => 'Australian mobiles (international)',   'regex' => '^00614[0-9]{8}$'),
       ),
       'Britain' => array(
-        array('label' => 'British landlines (national)',            'regex' => '^0[^7][0-9]{9}$'),
-        array('label' => 'British mobiles (national)',              'regex' => '^07[0-9]{9}$'),
+        array('label' => 'British landlines (national)',         'regex' => '^0[^7][0-9]{9}$'),
+        array('label' => 'British mobiles (national)',           'regex' => '^07[0-9]{9}$'),
         array('label' => 'British landlines (international)',    'regex' => '^0044[^7][0-9]{9}$'),
         array('label' => 'British mobiles (international)',      'regex' => '^00447[0-9]{9}$'),
       ),
       'Denmark' => array(
-        array('label' => 'Danish phones (national)',                'regex' => '^[0-9]{8}$'),
+        array('label' => 'Danish phones (national)',             'regex' => '^[0-9]{8}$'),
         array('label' => 'Danish phones (international)',        'regex' => '^0045[0-9]{8}$'),
       ),
       'France' => array(
-        array('label' => 'French landlines (national)',             'regex' => '^0[1|2|3|4|5|8|9][0-9]{8}$'), // 10 digits with 0 instead of 0033 (followed by non zero what is OR in regex?
-        array('label' => 'French mobiles (national)',               'regex' => '^0[6|7][0-9]{8}$'), // 06 and 07 are mobiles services
+        array('label' => 'French landlines (national)',          'regex' => '^0[1|2|3|4|5|8|9][0-9]{8}$'), // 10 digits with 0 instead of 0033 (followed by non zero what is OR in regex?
+        array('label' => 'French mobiles (national)',            'regex' => '^0[6|7][0-9]{8}$'), // 06 and 07 are mobiles services
         array('label' => 'French landlines (international)',     'regex' => '^0033[1|2|3|4|5|8|9][0-9]{8}$'), // cannot have 00 as 10 digit var
         array('label' => 'French mobiles (international)',       'regex' => '^0033[6|7][0-9]{8}$'),
       ),
       'Germany' => array(
-        array('label' => 'German phones (national)',                 'regex' => '^0[0-9]{8}$'), // TODO can we include mobiles v landlines distinction?
-        array('label' => 'German phones (international)',         'regex' => '^0049[0-9]{8}$'),
+        array('label' => 'German phones (national)',             'regex' => '^0[0-9]{8}$'), // TODO can we include mobiles v landlines distinction?
+        array('label' => 'German phones (international)',        'regex' => '^0049[0-9]{8}$'),
       ),
       'Ireland' => array(
-        array('label' => 'Irish phones (national)',                 'regex' => '^1[0-9]{7}$'), // TODO can we include mobiles v landlines distinction?
+        array('label' => 'Irish phones (national)',              'regex' => '^1[0-9]{7}$'), // TODO can we include mobiles v landlines distinction?
         array('label' => 'Irish phones (international)',         'regex' => '^00353[0-9]{7}$'),
       ),
       'The Netherlands' => array(
-        array('label' => 'Dutch phones (national)',                 'regex' => '^[0-9]{9}$'), // TODO can we include mobiles v landlines distinction?
+        array('label' => 'Dutch phones (national)',              'regex' => '^[0-9]{9}$'), // TODO can we include mobiles v landlines distinction?
         array('label' => 'Dutch phones (international)',         'regex' => '^0031[0-9]{9}$'),
       ),
       'Norway' => array(
-        array('label' => 'Norwegian landlines (national)',          'regex' => '^[^4|9][0-9]{7}$'),
-        array('label' => 'Norwegian mobiles (national)',            'regex' => '^[4|9][0-9]{7}$'),
+        array('label' => 'Norwegian landlines (national)',       'regex' => '^[^4|9][0-9]{7}$'),
+        array('label' => 'Norwegian mobiles (national)',         'regex' => '^[4|9][0-9]{7}$'),
         array('label' => 'Norwegian landlines (international)',  'regex' => '^0047[^4|9][0-9]{7}$'),
         array('label' => 'Norwegian mobiles (international)',    'regex' => '^0047[4|9][0-9]{7}$'),
       ),
@@ -55,19 +55,19 @@ class CRM_Phonenumbervalidator_Utils {
         array('label' => 'North American (America and Canada) (international)',  'regex' => '^001[2-9][0-9]{2}[2-9][0-9]{6}$'),
       ),        
       'Poland' => array(
-        array('label' => 'Polish landlines (national)',             'regex' => '^[5|6|7|8][0-9]{8}$'), // 9 digits.
-        array('label' => 'Polish mobiles (national)',               'regex' => '^[5|6|7|8][0-9]{8}$'), //  5, 6, 7 or 8 as lead indicate mobile
+        array('label' => 'Polish landlines (national)',          'regex' => '^[5|6|7|8][0-9]{8}$'), // 9 digits.
+        array('label' => 'Polish mobiles (national)',            'regex' => '^[5|6|7|8][0-9]{8}$'), //  5, 6, 7 or 8 as lead indicate mobile
         array('label' => 'Polish landlines (international)',     'regex' => '^0048[5|6|7|8][0-9]{8}$'),
         array('label' => 'Polish mobiles (international)',       'regex' => '^0048[5|6|7|8][0-9]{8}$'),
       ),
       'Spain' => array(
-        array('label' => 'Spanish landlines (national)',            'regex' => '^9[6|7][0-9]{8}$'), // 10 digits with not 6 or 7 as the second digit
-        array('label' => 'Spanish mobiles (national)',              'regex' => '^9[6|7][0-9]{8}$'),
+        array('label' => 'Spanish landlines (national)',         'regex' => '^9[6|7][0-9]{8}$'), // 10 digits with not 6 or 7 as the second digit
+        array('label' => 'Spanish mobiles (national)',           'regex' => '^9[6|7][0-9]{8}$'),
         array('label' => 'Spanish landlines (international)',    'regex' => '^00349[6|7][0-9]{8}$'),
         array('label' => 'Spanish mobiles (international)',      'regex' => '^00349[6|7][0-9]{8}$'),
       ),
       'Switzerland' => array(
-        array('label' => 'Swiss phones (national)',                 'regex' => '^0[0-9]{9}$'),
+        array('label' => 'Swiss phones (national)',              'regex' => '^0[0-9]{9}$'),
         array('label' => 'Swiss phones (international)',         'regex' => '^0041[0-9]{9}$'),
       ),
     );
