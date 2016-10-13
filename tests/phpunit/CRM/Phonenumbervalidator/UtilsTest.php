dcb9acee39ecf363b4b2d556d95ae7d3fcc9a962
John Kirk <john@civifirst.com>
2016-10-13 14:11:46 +0000

    Updated test headings

--- tests/phpunit/CRM/Phonenumbervalidator/UtilsTest.php
+++ tests/phpunit/CRM/Phonenumbervalidator/UtilsTest.php
@@ -5,16 +5,8 @@ use Civi\Test\HookInterface;
 use Civi\Test\TransactionalInterface;
 
 /**
- * FIXME - Add test description.
- *
- * Tips:
- *  - With HookInterface, you may implement CiviCRM hooks directly in the test class.
- *    Simply create corresponding functions (e.g. "hook_civicrm_post(...)" or similar).
- *  - With TransactionalInterface, any data changes made by setUp() or test****() functions will
- *    rollback automatically -- as long as you don't manipulate schema or truncate tables.
- *    If this test needs to manipulate schema or truncate tables, then either:
- *       a. Do all that using setupHeadless() and Civi\Test.
- *       b. Disable TransactionalInterface, and handle all setup/teardown yourself.
+ * This tests the utility functions of the Phone Number Validator: install, 
+ * uninstall, and rule matching.
  *
  * @group headless
  */
